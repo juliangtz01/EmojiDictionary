3be950ae28fb4688e3f15261d3a14caece4c7980
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2023-01-20 21:40:43 -0600

    added a class inside the EmmjiAdapter class, and added override functions

--- app/src/main/java/com/example/emojidictionary/EmojiAdapter.kt
+++ app/src/main/java/com/example/emojidictionary/EmojiAdapter.kt
@@ -1,6 +1,28 @@
 package com.example.emojidictionary
 
-class EmojiAdapter
-{
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
+class EmojiAdapter : RecyclerView.Adapter<EmojiAdapter.TextHolder>
+{
+    class TextHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener
+    {
+        override fun onClick(p0: View?) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return 10
+    }
+
+    override fun onBindViewHolder(holder: TextHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
