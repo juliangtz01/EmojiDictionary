8c0d1d53169ab72c86c947feda38d7b1af28643d
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2023-01-20 22:08:37 -0600

    removed the todo section and wrote code for the functions

--- app/src/main/java/com/example/emojidictionary/EmojiAdapter.kt
+++ app/src/main/java/com/example/emojidictionary/EmojiAdapter.kt
@@ -1,13 +1,22 @@
 package com.example.emojidictionary
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recyclerview_item_row.view.*
 
 class EmojiAdapter : RecyclerView.Adapter<EmojiAdapter.TextHolder>
 {
     class TextHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener
     {
+        var view : View = v
+        var emoji : String = ""
+        fun bindEmoji(emoji:String)
+        {
+            this.emoji = emoji
+            view.itemTextView.text = emoji
+        }
         override fun onClick(p0: View?) {
             TODO("Not yet implemented")
         }
@@ -15,14 +24,14 @@ class EmojiAdapter : RecyclerView.Adapter<EmojiAdapter.TextHolder>
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextHolder {
-        TODO("Not yet implemented")
+        return TextHolder(LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item_row, parent, false))
     }
 
     override fun getItemCount(): Int {
         return 10
     }
 
-    override fun onBindViewHolder(holder: TextHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: EmojiAdapter.TextHolder, position: Int) {
+        holder.bindEmoji("👍")
     }
 }
